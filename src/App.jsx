c0bbf59ecffe2aff7de3fabafd11df802b61e4aa
DhirajsGithub <borsedhiraj123@gmail.com>
2023-09-28 14:47:15 +0530

    basic model ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Chat from "./Components/Chat/Chat";
+import Signin from "./Components/Signin/Signin";
+import Signup from "./Components/Signup/Signup";
+
+function App() {
+  return (
+    <>
+      <Signin />
+    </>
+  );
+}
+
+export default App;
